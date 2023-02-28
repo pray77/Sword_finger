5f531be7cd1cf9051466357bc29708d7e5379adb
pray77 <74857519+pray77@users.noreply.github.com>
2023-03-01 01:14:44 +0800

    Create 快速排序.cpp

---
+++ 栈队列堆/最小的K个数/快速排序.cpp
@@ -0,0 +1,33 @@
+#include <queue>
+#include <vector>
+class Solution {
+public:
+    int partition(vector<int> &input, int l, int r) {
+        int pivot = input[l];
+        int i = l;
+        int j = r;
+        while(i < j) {
+            while(i < j && input[j] >= pivot) j--;
+            if(i < j) swap(input[i++], input[j]);
+            while(i < j && input[i] < pivot) i++;
+            if(i < j) swap(input[i], input[j--]);
+        }
+        return i;
+    }
+    void quicksort(vector<int> &input, int l, int r) {
+        int mid;
+        if (l < r) {
+            mid = partition(input, l, r);
+            quicksort(input, l, mid-1);
+            quicksort(input, mid+1, r);
+        }
+    }
+    priority_queue<int, vector<int>> pq;
+    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+        vector<int> result;
+        if (k ==0 || k > input.size()) return result;
+        int l = 0, r = input.size();
+        quicksort(input, l, r-1);
+        return vector<int>({input.begin(),input.begin()+k});
+    }
+};
